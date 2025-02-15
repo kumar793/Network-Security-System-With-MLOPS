3027c988b65f9e9da899f78be446cc86aeff216f
NAGENDRA KUMAR VOLETI <vnkumarvoleti@gmail.com>
2025-02-15 12:46:08 +0000

    Initial settings

---
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import numpy as np 
+import pandas as pd
+
+
+"""
+Defining common constant for training
+"""
+TARGET_COLUMN = "Result"
+PIPELINE_NAME:str = "NetworkSecurity"
+ARTIFACT_DIR:str = "Artifacts"
+FILE_NAME:str = "phishingData.csv"
+
+"""
+Data Ingestion related constants
+"""
+
+DATA_INGESTION_COLLECTION_NAME:str= "Network data"
+DATA_INGESTION_DATABASE_NAME:str = "Network-kumar"
+DATA_INGESTION_DIR_NAME:str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR:str = "feature_store"
+DATA_INGESTION_INGESTED_DIR:str = "ingested"
+DATA_INGESTION_TARIN_TEST_SPLIT_RATION:float = 0.2
