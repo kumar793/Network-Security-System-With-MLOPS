b6b449661c03b61ea398066cd8009d874295217d
NAGENDRA KUMAR VOLETI <vnkumarvoleti@gmail.com>
2025-02-18 11:46:42 +0000

    training pipeline built

---
+++ app.py
@@ -0,0 +1,54 @@
+import sys
+import os
+import certifi
+import pymongo
+from dotenv import load_dotenv
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+from networksecurity.pipeline.training_pipeline import TrainingPipeline
+from networksecurity.constants.training_pipeline import DATA_INGESTION_COLLECTION_NAME,DATA_INGESTION_DATABASE_NAME
+
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI,File,UploadFile,Request
+from uvicorn import run as app_run
+from fastapi.responses import Response
+from starlette.responses import RedirectResponse
+
+import pandas as pd
+
+from networksecurity.utils.main_utils.utils import load_object
+
+load_dotenv()
+mongo_db_url = os.getenv("MONGGO_DB_URI")
+ca = certifi.where()
+
+client = pymongo.MongoClient(mongo_db_url,tlsCAFile = ca)
+
+database = client[DATA_INGESTION_DATABASE_NAME]
+collection = database[DATA_INGESTION_COLLECTION_NAME]
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
+
+@app.get("/",tags = ["authentication"])
+async def index():
+    return RedirectResponse(url = "/docs")
+
+@app.get("/train")
+async def train_route():
+    try:
+        train_pipeline = TrainingPipeline
+        train_pipeline.run_pipeline()
+        return Response("Training is sucessful")
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+
